11bd30d57d5ff2f3ccbec3f44276691d27ed277f
Caio Andres <caioandresgit@outlook.com>
2026-01-17 23:42:13 -0300

    chore(zorro): ng zorro dependence installed.

--- apps/my-barbershop/src/app/app.config.ts
+++ apps/my-barbershop/src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideNzI18n, pt_BR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import pt from '@angular/common/locales/pt';
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
